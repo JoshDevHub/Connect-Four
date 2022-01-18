12a567f35e36b7fd48964564bd0841e8756931b3
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-18 15:41:03 -0700

    Add two passing tests for #user_selection method

---
+++ lib/player.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative 'display'
+
+# class that holds logic for the game's players
+class Player
+  include Display
+
+  def user_selection
+    selection = take_input
+    return selection if valid_selection?(selection)
+
+    # puts error_message(:not_in_range)
+    puts 'That input is not in the range of columns you can place your piece in. Try again.'
+    user_selection
+  end
+
+  def take_input
+    gets.chomp
+  end
+
+  def valid_selection?(selection)
+    ('1'..'7').include?(selection)
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,34 @@
 # frozen_string_literal: true
 
-describe Player do
+require_relative '../lib/player'
+
+describe Player do # rubocop: disable Metrics/BlockLength
   subject(:player) { described_class.new }
   describe '#user_selection' do
     context 'when user_selection is valid' do
-      xit 'returns 5' do
-        allow(player.take_input).to receive(:gets).and_return(5)
-        expect(player.user_selection).to eq(5)
+      before do
+        valid_input = '5'
+        allow(player).to receive(:gets).and_return(valid_input)
+      end
+
+      it 'displays no error message' do
+        error_message = 'That input is not in the range of columns you can place your piece in. Try again.'
+        expect(player).not_to receive(:puts).with(error_message)
+        player.user_selection
+      end
+    end
+
+    context 'when user_selection is invalid once, then valid' do
+      before do
+        letter = 'b'
+        valid_input = '7'
+        allow(player).to receive(:gets).and_return(letter, valid_input)
+      end
+
+      it 'displays error message once' do
+        error_message = 'That input is not in the range of columns you can place your piece in. Try again.'
+        expect(player).to receive(:puts).with(error_message).once
+        player.user_selection
       end
     end
   end
