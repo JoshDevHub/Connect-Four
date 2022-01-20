c00c67de14ec858868e7384f72b82b95b124286b
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-20 11:59:01 -0700

    feat: add methods to Board for initializing a @board_grid and reading its data

--- lib/board.rb
+++ lib/board.rb
@@ -2,5 +2,19 @@
 
 # class that represents the game board
 class Board
-  # placeholder
+  attr_reader :board_grid
+
+  def initialize
+    @board_grid = create_board
+  end
+
+  def create_board
+    board_hash = {}
+    6.times do |y|
+      7.times do |x|
+        board_hash[[x, y]] = nil
+      end
+    end
+    board_hash
+  end
 end
