c4794533f674f1b1e75a9fff9932c32648045143
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-20 15:40:45 -0700

    refactor: simplify #place_disc

--- lib/board.rb
+++ lib/board.rb
@@ -11,11 +11,10 @@ class Board
 
   # TODO: column full?
   def place_disc(color, column)
-    x_pos = column.to_i - 1
-    y_pos = 0
-    key_pointer = [x_pos, y_pos]
-    y_pos += 1 until board_grid[key_pointer].nil?
-    board_grid[key_pointer] = color
+    x = column.to_i - 1
+    y = 0
+    y += 1 until board_grid[[x, y]].nil?
+    board_grid[[x, y]] = color
   end
 
   private
