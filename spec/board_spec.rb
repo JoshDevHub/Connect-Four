9c2e6c48507ad8819ce1d3e3eab546a057fdad3a
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-20 12:18:56 -0700

    test-fix: place #place_disc call ahead of expect statement

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,15 +12,15 @@ describe Board do
         it 'places a red disc in column 1 of the board' do
           column = '1'
           coordinate = [0, 0]
-          expect(board.board_grid[coordinate]).to eq(disc_color)
           board.place_disc(disc_color, column)
+          expect(board.board_grid[coordinate]).to eq(disc_color)
         end
 
         it 'places a red disc in column 5 of the board' do
           column = '5'
           coordinate = [4, 0]
-          expect(board.board_grid[coordinate]).to eq(disc_color)
           board.place_disc(disc_color, column)
+          expect(board.board_grid[coordinate]).to eq(disc_color)
         end
       end
     end
