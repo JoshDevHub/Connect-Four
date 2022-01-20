617b72a00044ce31a81be664b327a508bea7dc99
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-20 13:55:15 -0700

    test: add test for checking yellow discs

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,10 +2,10 @@
 
 require_relative '../lib/board'
 
-describe Board do
+describe Board do # rubocop: disable Metrics/BlockLength
   subject(:board) { described_class.new }
 
-  describe '#place_disc' do
+  describe '#place_disc' do # rubocop: disable Metrics/BlockLength
     context 'when the board is empty' do
       context 'when the color is red' do
         disc_color = 'red'
@@ -19,6 +19,16 @@ describe Board do
         it 'places a red disc in column 5 of the board' do
           column = '5'
           coordinate = [4, 0]
+          board.place_disc(disc_color, column)
+          expect(board.board_grid[coordinate]).to eq(disc_color)
+        end
+      end
+
+      context 'when the color is yellow' do
+        disc_color = 'yellow'
+        it 'places a yellow disc in column 3 of the board' do
+          column = '3'
+          coordinate = [2, 0]
           board.place_disc(disc_color, column)
           expect(board.board_grid[coordinate]).to eq(disc_color)
         end
