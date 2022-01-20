3af92a8c506280c95c8cbb98e9356eb693f45f94
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-20 11:57:49 -0700

    tests: add tests for #place_disk method on the board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,25 @@
 # frozen_string_literal: true
 
+require_relative '../lib/board'
+
 describe Board do
-  # placeholder
+  subject(:board) { described_class.new }
+
+  describe '#place_disc' do
+    context 'when the color of the disc is red' do
+      disk_color = 'red'
+
+      it 'places a red disc at coordinate (2, 3) of the board' do
+        location = [2, 3]
+        expect(board.board_grid[location]).to eq(disk_color)
+        board.place_disc(disc_color, location)
+      end
+
+      it 'places a red disc at coordinate (5, 2) of the board' do
+        location = [5, 2]
+        expect(board.board_grid[location]).to eq(disk_color)
+        board.place_disc(disc_color, location)
+      end
+    end
+  end
 end
