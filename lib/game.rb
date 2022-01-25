40efe22f18ed94ff1dbe48078045c4c3ed0ee1fb
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-25 08:55:52 -0700

    planning: add pseudocode to #play_game for managing the game loop

--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,30 @@ class Game
   include Display
   attr_reader :game_board
 
-  def initialize(game_board)
+  def initialize(game_board:, player_one:, player_two:)
     @game_board = game_board
+    @player_one = player_one
+    @player_two = player_two
+  end
+
+  def play_game
+    # placeholder
+    # introduction & rules
+    # loop start
+    # -> player query
+    # -> get input + validate
+    # -> #place_disc
+    # -> if #connect_four? || #board_full?
+    # ----> end the loop & declare result
+    # -> run this loop with player2 (abs into own method)
+    # break when game is over
+    # ask if play again is desired
+    # reset and run game back if y else end
+  end
+
+  private
+
+  def reset_game
+    @game_board = game_board.new
   end
 end
