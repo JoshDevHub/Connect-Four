7cbea32b958c267d7fda20a0cb46443ef5d40a95
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-25 18:11:28 -0700

    feat: build out most functionality for Game. still needs refactor and tests

--- lib/game.rb
+++ lib/game.rb
@@ -3,11 +3,14 @@
 require_relative 'display'
 require_relative 'board'
 require_relative 'player'
+require_relative 'user_input'
+require 'pry-byebug'
 
 # class holding logic for running connect 4 games
 class Game
   include Display
-  attr_reader :game_board
+  include UserInput
+  attr_reader :game_board, :player_one, :player_two
 
   def initialize(game_board:, player_one:, player_two:)
     @game_board = game_board
@@ -18,42 +21,71 @@ class Game
   def play_game
     puts introduction
     loop do
-      until game_over?
-        game_loop(player_one)
-        game_loop(player_two)
+      current_player = player_one
+      loop do
+        # binding.pry
+        game_loop(current_player)
+        break if @game_over
+
+        current_player = current_player == player_one ? player_two : player_one
       end
       break unless play_again?
-
-      reset_game
     end
-    print_board(game_board.to_s)
-    # introduction & rules
-    # loop start
-    # -> player query
-    # -> get input + validate
-    # -> #place_disc
-    # -> if #connect_four? || #board_full?
-    # ----> end the loop & declare result
-    # -> run this loop with player2 (abs into own method)
-    # break when game is over
-    # ask if play again is desired
-    # reset and run game back if y else end
   end
 
   private
 
-  def game_over?
-    game_board.connect_four? || game_board.full?
+  def game_loop(player)
+    print_board(game_board.to_s)
+    puts query_message(:player_input)
+    player_choice = column_selection
+    game_board.place_disc(player.disc, player_choice)
+    check_game_over(player)
+  end
+
+  def column_selection
+    validate_user_input(&column_validator)
+  end
+
+  def column_validator
+    lambda do |input|
+      if game_board.column_full?(input)
+        puts error_message(:column_full)
+        false
+      elsif ('1'..'7').none?(input)
+        puts error_message(:not_in_range)
+      else
+        true
+      end
+    end
+  end
+
+  def check_game_over(player)
+    if game_board.connect_four?(player.disc)
+      @game_over = true
+      print_board(game_board.to_s)
+      puts "#{player.name} has won the game!"
+    elsif game_board.full_board?
+      @game_over = true
+      puts 'This game is a draw.'
+    end
   end
 
   def reset_game
     @game_board = game_board.new
+    @game_over = false
+  end
+
+  def play_again?
+    puts query_message(:play_again)
+    response = give_yes_no_input
+    response == 'y'
   end
 end
 
 game = Game.new(
   game_board: Board.new,
-  player_one: Player.new('🟡'),
-  player_two: Player.new('🔴')
+  player_one: Player.new('🟡', 'Player 1'),
+  player_two: Player.new('🔴', 'Player 2')
 )
 game.play_game
