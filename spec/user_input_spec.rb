7b2de3237553dea7cb7463d23213ae72b3197ffc
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-26 15:02:34 -0700

    test: add test that checks for one error message in #give_yes_no_input

--- spec/user_input_spec.rb
+++ spec/user_input_spec.rb
@@ -8,14 +8,37 @@ describe UserInput do
 
   describe '#give_yes_no_input' do
     context 'when the user input is valid' do
-      before do
-        input = 'y'
-        # allow(dummy_class).to receive(:error_message).and_return(error_message)
-        allow(dummy_class).to receive(:gets).and_return(input)
-      end
+      context 'when the user input is Y' do
+        before do
+          input = 'y'
+          # allow(dummy_class).to receive(:error_message).and_return(error_message)
+          allow(dummy_class).to receive(:gets).and_return(input)
+        end
 
-      it 'returns no error message' do
-        expect(dummy_class).not_to receive(:puts).with(error_message)
+        it 'returns no error message' do
+          expect(dummy_class).not_to receive(:puts).with(error_message)
+          dummy_class.give_yes_no_input
+        end
+      end
+      context 'when the user input is N' do
+        before do
+          input = 'n'
+          allow(dummy_class).to receive(:gets).and_return(input)
+        end
+        it 'returns no error message' do
+          expect(dummy_class).not_to receive(:puts).with(error_message)
+          dummy_class.give_yes_no_input
+        end
+      end
+    end
+    context 'when the user input is invalid once and then valid' do
+      before do
+        invalid_letter = 'a'
+        valid_input = 'y'
+        allow(dummy_class).to receive(:gets).and_return(invalid_letter, valid_input)
+      end
+      it 'returns an error message once' do
+        expect(dummy_class).to receive(:puts).with(error_message).once
         dummy_class.give_yes_no_input
       end
     end
