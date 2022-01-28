32e569833fd88dd2b40f7a5bf58f3e7e733b61a5
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-28 10:38:18 -0700

    feat: build out methods for allowing the user to input a name

--- lib/player.rb
+++ lib/player.rb
@@ -6,14 +6,37 @@ require_relative 'user_input'
 # class that holds logic for the game's players
 class Player
   include Display
+  include UserInput
   attr_reader :disc, :name
 
-  def initialize(disc_token: nil, name: nil)
+  @player_counter = 0
+
+  def initialize(disc_token: nil)
     @disc = disc_token
-    @name = name
+    @name = "Player #{self.class.count}"
+  end
+
+  def input_name
+    puts "#{name}, #{query_message(:player_name)}"
+    user_name = validate_user_input(&name_validator)
+    @name = user_name
   end
 
   def to_s
     name
+  end
+
+  def self.count
+    @player_counter += 1
+  end
+
+  def name_validator
+    lambda do |input|
+      if /^[a-zA-Z]+$/.match?(input)
+        true
+      else
+        puts error_message(:invalid_name)
+      end
+    end
   end
 end
