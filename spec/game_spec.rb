f96ef3c8a764af18c47c2e19d164b4de96b333a9
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-28 08:49:34 -0700

    chore: add spacing in game_spec file

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,6 +5,7 @@ require_relative '../lib/game'
 describe ConnectFour::Game do
   let(:player_one) { double(name: 'Player 1', disc: '🟡') }
   let(:player_two) { double(name: 'Player 2', disc: '🔴') }
+
   describe '#play_game' do
     let(:game_board) { double('game_board') }
     subject(:game) do
@@ -14,17 +15,20 @@ describe ConnectFour::Game do
         game_board: game_board
       )
     end
+
     context 'when the user(s) plays one game of Connect Four' do
       before do
         allow(game).to receive(:puts)
         quit = 'n'
         allow(game).to receive_message_chain(:gets, :chomp).and_return(quit)
       end
+
       it 'calls game_loop one time' do
         expect(game).to receive(:game_loop).once
         game.play_game
       end
     end
+
     context 'when the user(s) play two games of Connect Four' do
       before do
         allow(game).to receive(:puts)
@@ -32,11 +36,13 @@ describe ConnectFour::Game do
         quit = 'n'
         allow(game).to receive_message_chain(:gets, :chomp).and_return(play_again, quit)
       end
+
       it 'calls game_loop twice' do
         expect(game).to receive(:game_loop).twice
         game.play_game
       end
     end
+
     context 'when the user(s) play five games of Connect Four' do
       before do
         allow(game).to receive(:puts)
@@ -44,12 +50,14 @@ describe ConnectFour::Game do
         quit = 'n'
         allow(game).to receive_message_chain(:gets, :chomp).and_return(play_again, play_again, play_again, play_again, quit)
       end
+
       it 'calls game_loop five times' do
         expect(game).to receive(:game_loop).exactly(5).times
         game.play_game
       end
     end
   end
+
   describe '#game_loop' do
     context 'when the game is over from connect four' do
       let(:game_board) { double(column_full?: false, full_board?: false, place_disc: nil) }
@@ -65,6 +73,7 @@ describe ConnectFour::Game do
         allow(connect_four_game).to receive(:puts)
         allow(connect_four_game).to receive_message_chain(:gets, :chomp).and_return('1')
       end
+
       it 'calls column_selection four times' do
         expect(connect_four_game).to receive(:column_selection).exactly(4).times
         connect_four_game.game_loop
@@ -86,12 +95,14 @@ describe ConnectFour::Game do
         allow(full_board_game).to receive(:puts)
         allow(full_board_game).to receive_message_chain(:gets, :chomp).and_return('1')
       end
+
       it 'calls column_selection forty-two times' do
         expect(full_board_game).to receive(:column_selection).exactly(42).times
         full_board_game.game_loop
       end
     end
   end
+
   describe '#play_again?' do
     let(:game_board) { double('game_board') }
     subject(:game) do
@@ -108,21 +119,25 @@ describe ConnectFour::Game do
           user_input = 'y'
           allow(game).to receive(:gets).and_return(user_input)
         end
+
         it 'returns true' do
           expect(game.play_again?).to be(true)
         end
       end
+
       context "when the user input is 'n'" do
         before do
           allow(game).to receive(:puts)
           user_input = 'n'
           allow(game).to receive(:gets).and_return(user_input)
         end
+
         it 'returns false' do
           expect(game.play_again?).to be(false)
         end
       end
     end
+
     context 'when the user input is invalid once' do
       before do
         allow(game).to receive(:puts)
@@ -130,6 +145,7 @@ describe ConnectFour::Game do
         valid_input = 'y'
         allow(game).to receive(:gets).and_return(invalid_input, valid_input)
       end
+
       it "returns true with valid 'y' input" do
         expect(game.play_again?).to be(true)
       end
