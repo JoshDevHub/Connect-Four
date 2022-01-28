57439ff5ad0d9adc52ae23453771f82d56d89604
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-28 10:45:56 -0700

    test: add tests for checking that only valid names are accepted

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -32,17 +32,33 @@ describe Player do
       end
     end
 
-    context 'when the user inputs an invalid name once' do
+    context 'when the user inputs a name with non-alphabetical chars' do
       subject(:player) { described_class.new }
       before do
         allow(player).to receive(:puts)
-        invalid_input = '123'
+        non_alpha_input = '123'
         valid_input = 'player'
-        allow(player).to receive(:gets).and_return(invalid_input, valid_input)
+        allow(player).to receive(:gets).and_return(non_alpha_input, valid_input)
       end
 
-      xit "runs twice" do
-        expect(player).to receive(:gets).twice
+      it 'sets name to the valid name' do
+        valid_name = 'player'
+        expect { player.input_name }.to change { player.name }.to(valid_name)
+      end
+    end
+
+    context 'when the user inputs a name that is too long' do
+      subject(:long_name) { described_class.new }
+      before do
+        allow(long_name).to receive(:puts)
+        long_input = 'abcdefghijklmnopqrstuvwxyz'
+        valid_input = 'player'
+        allow(long_name).to receive(:gets).and_return(long_input, valid_input)
+      end
+
+      it 'sets name to the valid name' do
+        valid_name = 'player'
+        expect { long_name.input_name }.to change { long_name.name }.to(valid_name)
       end
     end
   end
